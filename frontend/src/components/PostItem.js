8df56bb633059fa839d0ca60a3b75e0caedb01aa
Flor Antara <florenciaantara@gmail.com>
2018-03-08 23:36:09 -0800

    Enhancement: PostItem to its own component

---
+++ frontend/src/components/PostItem.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { postDelete } from '../actions/Post'
+import { Link } from 'react-router-dom'
+import PostMeta from '../components/PostMeta'
+import FaTrash from 'react-icons/lib/fa/trash'
+
+const PostItem = ({post, postDelete}) => {
+
+    const { timestamp, category, id, title, author, voteScore, commentCount } = post
+
+    // Delete Post
+    const onDeletePost = (id) =>{
+        postDelete(id)
+    }
+
+    const date = new Date(timestamp)
+
+    return(
+        <div className="PostsList-item" key={id}>
+            <h4>
+                <Link to={`/${category}/${id}`}>{title} </Link>
+                <small>by {author} on {date.toDateString()}</small>
+            </h4>
+            <PostMeta
+                voteScore={voteScore}
+                commentCount={commentCount}
+                id={id}
+                context="PostsList"
+            />
+            <FaTrash onClick={() => onDeletePost(id)}/>
+
+        </div>
+    )
+}
+
+
+const mapDispatchToProps = dispatch => ({
+    dispatch,
+    postDelete: (id) => dispatch(postDelete(id))
+})
+
+export default connect(null, mapDispatchToProps)(PostItem)
--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -1,49 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import PostMeta from '../components/PostMeta'
-import FaTrash from 'react-icons/lib/fa/trash'
+import PostItem from './PostItem'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { postDelete } from '../actions/Post'
 
-const PostsList = ({ posts, postDelete }) => {
 
-    // Delete Post
-    const onDeletePost = (id) =>{
-        postDelete(id)
-    }
+const PostsList = ({posts}) => {
+
     return(
         <div className="PostsList">
             { posts && posts.map( post => {
-                const date = new Date(post.timestamp)
                 return (
-                        <div className="PostsList-item" key={post.id}>
-                            <h4>
-                                <Link to={`/${post.category}/${post.id}`}>{post.title} </Link>
-                                <small>by {post.author} on {date.toDateString()}</small>
-                            </h4>
-                            <PostMeta
-                                voteScore={post.voteScore}
-                                commentCount={post.commentCount}
-                                id={post.id}
-                                context="PostsList"
-                             />
-                              <FaTrash onClick={() => onDeletePost(post.id)}/>
-                        </div>
-                    )
-                } )
+                    <PostItem key={post.id} post={post}/>
+                )
+            } )
             }
         </div>
     )
+
 }
 
 PostsList.propTypes = {
     posts: PropTypes.array.isRequired
 }
 
-const mapDispatchToProps = dispatch => ({
-    dispatch,
-    postDelete: (id) => dispatch(postDelete(id))
-})
-
-export default connect(null, mapDispatchToProps)(PostsList)
+export default PostsList
